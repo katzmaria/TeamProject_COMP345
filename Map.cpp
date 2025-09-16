4150799a100bc1b7efecbf477bd23744ebc2333d
Abdullah <abdullahtahad30@gmail.com>
2025-09-15 21:20:06 -0400

    feat: added more functions including parsing and retreiving the continent names

--- Map.cpp
+++ Map.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <string>
 #include <ostream>
+#include <iostream>
 
+bool continentFlag = false;
 
 bool MapLoader::load(const std::string& filepath, Map*& outMap, std::ostream& diag) {
 	outMap = nullptr; // we dont have anything to build right now 
@@ -17,12 +19,61 @@ bool MapLoader::load(const std::string& filepath, Map*& outMap, std::ostream& di
 	int total = 0; 
 	std::string line; 
 
+	Map* m = new Map();
+	int nextContId = 0;
+
 	while (std::getline(in, line)) { // here we are using the getLine() method to read line by line what is happening
+		if (line == "[Continents]"){
+			std::cout << "we found the continent! \n \n";
+			continentFlag = true;
+			continue;
+		}
+
+		if (continentFlag == true && !line.empty()) {
+			addContinentFromLine(m, line);
+		}
+		else {
+			continentFlag = false;
+		}
+
 		++total;
 	}
 
+	outMap = m;
+
+  // std::cout << outMap->continents[0].continentName << "\n";
+
+
 	diag << "[loader] opened OK. total lines = " << total << "\n";
 	return true;
-
-
 }
+
+void MapLoader::addContinentFromLine(Map* m, const std::string& line) {
+	std::string continentName = "";
+
+	for (char ch : line) {
+		if (ch != '=') {
+			continentName += ch;
+		}
+		else {
+			break;
+		}
+
+	}
+	// continent should be good
+	std::cout << "adding continent ----> " << continentName << " \n";
+	int id = (*m).addContinent(continentName);
+}
+
+int Map::addContinent(const std::string& continentName) {
+	int id = static_cast<int>(continents.size());
+
+	Continent c; 
+	c.continentID = id; 
+	c.continentName = continentName;
+
+	continents.push_back(c);
+	return id;
+}
+
+
--- Map.h
+++ Map.h
@@ -2,10 +2,37 @@
 #define MAP_H
 
 #include <string>
+#include <vector>
 #include <ostream>
 
-class Map; // here we have a forward declaration basically telling the compiler we have a class named Map
+struct Territory {
+	int id = -1;
+	int continentID = -1;
+	std::string name = "";
+	int x;
+	int y;
+	std::vector<std::string> neighborsNames;
+};
+
+struct Continent {
+	int continentID = -1;
+	std::string continentName = "";
+};
+
+
+class Map {
+public:
+	std::vector<Territory> territories;
+	std::vector<Continent> continents;
+
+public:
+	int addContinent(const std::string& continentName); // here the & operator is used because we want to use the same string and not a copy
+
+};
+
+
 class MapLoader {
+	void addContinentFromLine(Map* m, const std::string& line);
 public:
 	bool load(const std::string& filepath, Map*& outMap, std::ostream& diag);
 	// we are adding a & to string because dont want to copy the string but just get the reference, this should be bit more efficient
@@ -13,6 +40,7 @@ public:
 		// we add a & to refer to the caller's pointer variable itself (thus not a direct copy)
 	// again the & is used because we want to write directly to the object itself and not just a copy
 };
+
 
 
 #endif
