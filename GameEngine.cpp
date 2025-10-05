ea341a313f4a48d5f446f615c98e8df7cb6aa5a8
Carson <carsontwj@gmail.com>
2025-10-04 23:20:01 -0400

    Add constructors/destructor for GameEngine

---
+++ GameEngine.cpp
@@ -0,0 +1,33 @@
+#include "GameEngine.h"
+#include <string>
+#include <iostream>
+
+// default constructor will initialize the GameEngine object to the start state
+GameEngine::GameEngine() {
+    currentState = new std::string("start");
+}
+
+// copy constructor 
+GameEngine::GameEngine(const GameEngine& engine){
+    currentState = new std::string(*engine.currentState);
+}
+
+// Assignment operator
+GameEngine& GameEngine::operator=(const GameEngine& engine){
+    if (this != &engine){
+        delete currentState;
+        currentState = new std::string(*engine.currentState);
+    }
+    return *this;
+}
+
+// Stream insertion operator implementation
+std::ostream& operator<<(std::ostream& out, const GameEngine& engine){
+    out << "Current Game State: " << *engine.currentState;
+    return out;
+}
+
+// destructor *(will have to be updated when we add more attributes to the class)
+GameEngine::~GameEngine(){
+    delete currentState;
+}
---
+++ GameEngine.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class GameEngine {
+    private:
+        std::string* currentState;
+
+    public: 
+        GameEngine();
+        GameEngine(const GameEngine& engine);
+        GameEngine& operator=(const GameEngine& engine);
+        friend std::ostream& operator<<(std::ostream& out, const GameEngine& engine);
+        ~GameEngine();
+};
