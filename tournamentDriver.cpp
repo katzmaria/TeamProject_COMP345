743a11f2b5b96f6545b4f7285284ea3f51f113fa
Carson <carsontwj@gmail.com>
2025-11-23 00:18:36 -0500

    comment added

--- tournamentDriver.cpp
+++ tournamentDriver.cpp
@@ -2,6 +2,7 @@
 #include "GameEngine.h"
 
 void testTournament() {
+    // can test with this command: "tournament -M 002_I72_X-29.map 001_I72_Ghtroc 720.map -P aggressive cheater neutral benevolent -G 2 -D 20"
     GameEngine engine = GameEngine();
     std::cout << "Enter a valid tournament command: ";
     engine.startupPhase();
